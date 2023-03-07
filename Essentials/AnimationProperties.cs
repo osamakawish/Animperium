2ebf3904f916017d9bf3b9d712738fa26a9cd812
Osama Kawish <osamakawish@gmail.com>
2023-03-07 09:41:46 -0500

    Implemented hashet as well for animation properties class.

--- Essentials/AnimationProperties.cs
+++ Essentials/AnimationProperties.cs
@@ -19,6 +19,7 @@ internal record AnimationProperty(Shape Shape, DependencyProperty Property, Anim
 internal class AnimationProperties : ICollection<AnimationProperty>
 {
     private readonly PropertyAnimationDictionary _dictionary = new();
+    private readonly HashSet<AnimationProperty> _set = new();
 
     public IEnumerator<AnimationProperty> GetEnumerator()
         => _dictionary.SelectMany(x => x.Value
@@ -33,23 +34,51 @@ internal class AnimationProperties : ICollection<AnimationProperty>
             _dictionary.Add(shape, new Dictionary<DependencyProperty, AnimationTimeline>());
 
         _dictionary[shape].Add(property, animation);
+        _set.Add(new AnimationProperty(shape, property, animation));
     }
     public void Add(AnimationProperty item) => Add(item.Shape, item.Property, item.Animation);
 
-    public void Clear() => _dictionary.Clear();
-    public bool Contains(AnimationProperty item) => false;
+    public void Clear()
+    {
+        _dictionary.Clear(); 
+    }
+
+    public bool Contains(AnimationProperty item)
+        => _dictionary.ContainsKey(item.Shape)
+           && _dictionary[item.Shape].TryGetValue(item.Property, out var value)
+           && item.Animation == value;
 
     public void CopyTo(AnimationProperty[] array, int arrayIndex)
     {
         foreach (var property in this)
             if (arrayIndex <= array.Length) array[arrayIndex++] = property;
     }
-    public bool Remove(AnimationProperty item) => false;
+    public bool Remove(AnimationProperty item)
+    {
+        if (!_set.Contains(item)) return false;
+
+        var animationTimelines = _dictionary[item.Shape];
+        animationTimelines.Remove(item.Property);
+        if (animationTimelines.Count == 0) _dictionary.Remove(item.Shape);
+
+        _set.Remove(item);
+
+        return true;
+    }
 
     public int Count => _dictionary.Values.Sum(animation => animation.Count);
 
     public void ForEach(Action<AnimationProperty> action)
-    { foreach (var property in this) action(property); }
+    { foreach (var animationProperty in _set) action(animationProperty); }
+
+    public void ForEach(Action<Shape, DependencyProperty,  AnimationTimeline> action)
+    {
+        foreach (var (shape, animationTimelines) in _dictionary) {
+            foreach (var (property, animation) in animationTimelines) {
+                action(shape, property, animation);
+            }
+        }
+    }
 
     public bool IsReadOnly => false;
 }
